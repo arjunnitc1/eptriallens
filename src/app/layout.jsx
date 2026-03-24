48e9edcec5c28f9481cd0db8378edef1b47545a4
arjun <arjun.nitclicut@gmail.com>
2026-03-24 13:00:07 +0000

    Zoom issues in mobile

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,6 +7,13 @@ export const metadata = {
     icons: { icon: '/logo.png' },
 };
 
+export const viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+};
+
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
